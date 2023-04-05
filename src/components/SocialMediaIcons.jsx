451720e346b699397f744a67a78e438995b1246f
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2023-04-05 09:57:18 +0530

    first commit

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,20 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a className="hover:opacity-50 transition duration-500" target="_blank" href="https://www.linkedin.com/in/utkarsh-v-50395184/" rel="noreferrer">
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+      <a className="hover:opacity-50 transition duration-500" target="_blank" href="https://www.linkedin.com/in/utkarsh-v-50395184/" rel="noreferrer">
+        <img alt="twitter-link" src="../assets/twitter.png" />
+      </a>
+      <a className="hover:opacity-50 transition duration-500" target="_blank" href="https://www.linkedin.com/in/utkarsh-v-50395184/" rel="noreferrer">
+        <img alt="facebook-link" src="../assets/facebook.png" />
+      </a>
+      <a className="hover:opacity-50 transition duration-500" target="_blank" href="https://www.linkedin.com/in/utkarsh-v-50395184/" rel="noreferrer">
+        <img alt="instagram-link" src="../assets/instagram.png" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
